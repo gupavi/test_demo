6846671d6da6223214583d7dc3eebb136ca7fc7f
gupavi <abhinav200002@gmail.com>
2021-01-28 23:21:14 +0530

    deleted app1.js

--- app1.js
+++
@@ -1,6 +0,0 @@
-var logger = require("./logger1_export_module");
-console.log(logger);
-logger.mylogfunction("Import successful");
-
-// const logger = require("./logger");//good practice
-// logger = 1;//compilation error
