f4bbabaaa269b0c0a52e560b5a89962b05dfc444
gupavi <abhinav200002@gmail.com>
2021-01-28 23:35:47 +0530

    Revert "added app2.js"
    This reverts commit a4a9475eb9b8fd59d141c5daf9194eabaab432a8.

--- app2_path.js
+++
@@ -1,5 +0,0 @@
-const path = require('path');
-var pathObj = path.parse(__filename);
-console.log(pathObj);
-//tells the root, directory path, name of file, extension of the file, name of the file without extension
-//use path module instead of string to deal with "path" related issues.
