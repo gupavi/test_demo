f6aec4842e22748bd32e0f1801b8df5095c853f8
gupavi <61126917+gupavi@users.noreply.github.com>
2021-01-29 00:24:42 +0530

    Delete app3_os.js

--- app3_os.js
+++
@@ -1,8 +0,0 @@
-const os = require('os');
-var totalMemory = os.totalmem();
-var freeMemory = os.freemem();
-// console.log("totalMemory " + totalMemory);
-
-// template string: ES8: ecma script 8
-console.log(`Total Memory:  ${totalMemory}`);
-console.log(`Free Memory:  ${freeMemory}`);
